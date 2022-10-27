57d006bb68e0bb13bc7c1c8febf82e2c5f14f630
Rafael de Souza Freire <rsouzaf@pop-os.localdomain>
2022-10-27 19:47:52 -0300

    First component- navbar

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Home from './pages/Home'
+import './global.css'
+
+function App() {
+
+  return (
+   <>
+    <Home/>
+   </>
+  )
+}
+
+export default App
---
+++ client/src/Component/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Search, ShoppingCart } from '@material-ui/icons'
+
+export default function Navbar() {
+  return (
+    <div className='flex items-center justify-between text-[#F7FFF7] bg-[#0B022D] px-7 py-3 '>      
+      <div className='text-[#0B022D] rounded bg-slate-600 p-2 '>  
+        <input type='text' className=' w-60 bg-slate-600' />
+        <Search/>
+      </div>
+      <img src="./src/assets/logoSub.png" alt="" className='h-16 text-center ' />
+      <div className='flex  items-center '>
+        <ShoppingCart/>
+        <button className=' bg-slate-500 rounded p-2 mx-5 justify-end'>Cadastrar</button>
+        <button className='bg-slate-500 rounded p-2  justify-end'>Entrar</button>
+      </div>
+    </div>
+  )
+}
