1b4787e93077366ec5eb6a2dc0824ee4f9bca46c
Rafael de Souza Freire <rsouzaf@pop-os.localdomain>
2022-11-01 11:39:36 -0300

    adding responsivity to the navbar

--- client/src/Component/Navbar.tsx
+++ client/src/Component/Navbar.tsx
@@ -1,19 +1,46 @@
 import React from 'react'
-import { Search, ShoppingCart } from '@material-ui/icons'
+import { Menu, Search, ShoppingCart } from '@material-ui/icons'
 
 export default function Navbar() {
   return (
-    <div className='flex items-center justify-between text-[#F7FFF7] bg-[#0B022D] px-7 py-3 '>      
-      <div className='text-[#0B022D] rounded bg-slate-600 p-2 '>  
-        <input type='text' className=' w-60 bg-slate-600' />
-        <Search className='cursor-pointer'/>
+    <section>
+
+      <div className='flex items-center justify-between text-[#F7FFF7] bg-[#0B022D] px-7 py-3 '>      
+
+      {/* Navbar search */ }
+        <div className='hidden md:flex text-[#0B022D] rounded bg-slate-600 p-2 '>  
+          <input type='text' className=' w-60 bg-slate-600' />
+          <Search className='cursor-pointer'/>
+        </div>
+        
+        {/* Logo */ }
+        <img src="./src/assets/logoSub.png" className='h-14 md:h-16 text-center ' />
+        
+        {/* Navbar buttons */ }
+
+        <div className='hidden  md:flex  items-center '> 
+          <ShoppingCart className='cursor-pointer'/>
+          <button className=' bg-slate-500 rounded p-2 mx-5 justify-end'>Cadastrar</button>
+          <button className='bg-slate-500 rounded p-2  justify-end'>Entrar</button>
+        </div>
+
+        {/* Navbar menu with responsivity */ }
+        <div className='md:hidden flex items-center'>
+          <ShoppingCart className='cursor-pointer mx-2'/>
+          <Menu className='cursor-pointer'/>
+        </div>        
       </div>
-      <img src="./src/assets/logoSub.png" alt="" className='h-16 text-center ' />
-      <div className='flex  items-center '> 
-        <ShoppingCart className='cursor-pointer'/>
-        <button className=' bg-slate-500 rounded p-2 mx-5 justify-end'>Cadastrar</button>
-        <button className='bg-slate-500 rounded p-2  justify-end'>Entrar</button>
+
+      {/* mobile buttons in menu */}
+
+      <div className=' text-[#F7FFF7] bg-[#0B022D]'>
+        <div className='flex justify-center '>
+          <input type='text' className='w-3/4 bg-[#64748B] rounded-md' />
+          <Search className='mx-2'/>
+        </div>
+        <p className='p-2 cursor-pointer hover:opacity-70'>Cadastrar</p>
+        <p className='p-2 cursor-pointer hover:opacity-70'>Entrar</p>
       </div>
-    </div>
+    </section>
   )
 }
